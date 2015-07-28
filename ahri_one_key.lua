543c7019a72f951832aa2d805a86bd4139056043
krispycream <reysanchez404@gmail.com>
2015-07-29 03:14:32 +0800

    Update ahri_one_key.lua

--- ahri_one_key.lua
+++ ahri_one_key.lua
@@ -2,47 +2,36 @@ require("Inspired")
 require('IWalk')
 
 
-AddInfo("Ahri", "Ahri")
-AddButton("Q", "Use Q", true)
-AddButton("W", "Use W", true)
-AddButton("E", "Use E", true)
-AddButton("R", "R mouse position", true)
+Config = scriptConfig("Ahri", "Ahri_One_Key")
+Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("R", "Use R mouse position", SCRIPT_PARAM_ONOFF, true)
 
 
 OnLoop(function(myHero)
-	IWalk()
+	if not IWalkConfig.Combo then return end
 	DrawMenu()
 	DrawText("ONE KEY TO WIN",24,0,0,0xffff0000);
-	if GetKeyValue("Combo") then   
 	local target = GetCurrentTarget()
 	   if ValidTarget(target, 1000) then
 
 		local QPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),2500,250,1000,100,false,false)
-			if GetButtonValue("Q") then
                  	if CanUseSpell(myHero, _Q) == READY and QPred.HitChance == 1 then
                  	CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 		end
-end
-		if GetButtonValue("W") then
-		if  CanUseSpell(myHero, _W) == READY and IsInDistance(target, 600) then
+		
+		if  CanUseSpell(myHero, _W) == READY and Config.Q and IsInDistance(target, 600) then
                         CastTargetSpell(myHero,_W)
 			end
-end
-		if ValidTarget(target, 1000) then
 		local EPred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target),1550,250,1000,100,true,true)	
-			if GetButtonValue("E") then
-	        	if CanUseSpell(myHero, _E) == READY and QPred.HitChance == 1 then
+	        	if CanUseSpell(myHero, _E) == READY and Config.E and EPred.HitChance == 1 then
                  	CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 		 		end
-                	end
-end
 
-			if GetButtonValue("R") then
 			local mousePos = GetMousePos()
-			if CanUseSpell(myHero, _R) == READY then
+			if CanUseSpell(myHero, _R) == READY and Config.R then
 			   CastSkillShot(_R,mousePos.x,mousePos.y,mousePos.z)
 	end
-end
 	    	end 
-	end
 end)

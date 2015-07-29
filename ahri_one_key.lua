e94ebcfdc174bcdfacecfe9f1c379643980c4d40
krispycream <reysanchez404@gmail.com>
2015-07-29 10:26:58 +0800

    Update ahri_one_key.lua

--- ahri_one_key.lua
+++ ahri_one_key.lua
@@ -1,7 +1,3 @@
-require("Inspired")
-require('IWalk')
-
-
 Config = scriptConfig("Ahri", "Ahri_One_Key")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)

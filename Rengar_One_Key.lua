acb03106effbeff2d7b8efa11ba0b2cba733184e
krispycream <reysanchez404@gmail.com>
2015-07-30 14:53:44 +0800

    Update Rengar_One_Key.lua

--- Rengar_One_Key.lua
+++ Rengar_One_Key.lua
@@ -8,7 +8,7 @@ PrintChat(textTable[2])
 PrintChat(textTable[3])
 PrintChat(textTable[4]) 
 
-Config = scriptConfig("Rengar", "Rengar_One_Key Loaded")
+Config = scriptConfig("Rengar", "Rengar_One_Key")
 Config.addParam("Q", "Use Q", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("W", "Use W", SCRIPT_PARAM_ONOFF, true)
 Config.addParam("E", "Use E", SCRIPT_PARAM_ONOFF, true)
